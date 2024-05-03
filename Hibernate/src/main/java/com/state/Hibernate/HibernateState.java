afbf170f1d1f2a45ccfdabb92996727ebd9bf703
Yash <Yash.mp@rediffmail.com>
2024-05-03 19:07:13 +0530

    Do needfull changeg

--- Hibernate/src/main/java/com/state/Hibernate/HibernateState.java
+++ Hibernate/src/main/java/com/state/Hibernate/HibernateState.java
@@ -1,9 +1,16 @@
 package com.state.Hibernate;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class HibernateState {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Configuration configuration=new Configuration();
+		configuration.configure("hibernate.cfg.xml");
+		SessionFactory sessionFactory=configuration.buildSessionFactory();
+		Session session=sessionFactory.openSession(); 
 
 	}
 
